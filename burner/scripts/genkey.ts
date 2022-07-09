e613d42782a8e05dc253b23012ae58ea548638f5
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-07-09 21:27:18 +0200

    update pull request template

--- burner/scripts/genkey.ts
+++ burner/scripts/genkey.ts
@@ -2,8 +2,8 @@ import { ec } from 'starknet';
 
 const keypair = ec.genKeyPair();
 
-console.log('-----------------------------------------------------');
+console.log('--------------------------------------------------------------------------------');
 console.log(`privatekey: 0x${keypair.getPrivate().toString('hex')}`);
 let keys = keypair.getPublic();
 console.log(`publickey:  ${ec.getStarkKey(keypair)}`);
-console.log('-----------------------------------------------------');
+console.log('--------------------------------------------------------------------------------');
