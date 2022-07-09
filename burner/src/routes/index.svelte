393aaa3f6c51748e7f737bef224a3f72eece2d79
Gregory Guillou <10611760+gregoryguillou@users.noreply.github.com>
2022-07-09 23:34:33 +0200

    generate a starknet private/public 🔐 key pair (#10)

--- burner/src/routes/index.svelte
+++ burner/src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+	import Burner from '$lib/Burner.svelte';
+</script>
+
+<div>
+	<Burner />
+</div>
