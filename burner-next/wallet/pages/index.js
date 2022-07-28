2c725a8c86505a44f9065d4c445b6a1c4b715db6
0xJilan <0xjilan@protonmail.com>
2022-07-28 14:55:44 +0200

    add key with local state

--- burner-next/wallet/pages/index.js
+++ burner-next/wallet/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Router from "next/router";
 import styles from "styles/Home.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStateContext } from "lib/context";
 import { getLocalStorage, saveLocalStorage } from "lib/handleLocalStorage";
 import { generateKey } from "lib/handleKey";
@@ -9,6 +9,7 @@ import Loader from "components/Loader";
 
 export default function Home() {
   const [state, setState] = useStateContext();
+  const [key, setKey] = useState("");
 
   useEffect(() => {
     if (window) {
@@ -25,6 +26,7 @@ export default function Home() {
         console.log("there is session key");
         console.log(" session key : ", sessionKey);
         setState("INITIALIZED");
+        setKey(sessionKey);
       }
     }
   }, []);
@@ -37,7 +39,12 @@ export default function Home() {
       </Head>
       <main className={styles.main} style={{ width: "100vw", height: "100vh" }}>
         {state === "UNINITIALIZED" && <Loader message="Key generation..." />}
-        {state === "INITIALIZED" && <h1>Hello</h1>}
+        {state === "INITIALIZED" && (
+          <>
+            <h1>Hello</h1>
+            <span>key is {key}</span>
+          </>
+        )}
       </main>
     </div>
   );
