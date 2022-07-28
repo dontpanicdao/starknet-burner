cd5551078ee20826d666de2a1ba545e72eea9f3f
0xJilan <0xjilan@protonmail.com>
2022-07-28 11:26:36 +0200

    remove useless testing code and add absolute import in jsconfig

--- burner-next/wallet/lib/handleKey.js
+++ burner-next/wallet/lib/handleKey.js
@@ -3,9 +3,13 @@ import { toBN } from "starknet/utils/number";
 
 const generateKey = () => {
   const sessionKey = `0x${ec.genKeyPair().getPrivate().toString(16)}`;
-  const keypair = ec.getKeyPair(toBN(sessPrivateKey));
-  const sessionPublicKey = ec.getStarkKey(keypair);
   return sessionKey;
 };
 
-export { generateKey };
+const getPublicKey = (sessionKey) => {
+  const keypair = ec.getKeyPair(toBN(sessionKey));
+  const sessionPublicKey = ec.getStarkKey(keypair);
+  return sessionPublicKey;
+};
+
+export { generateKey, getPublicKey };
--- burner-next/wallet/pages/index.js
+++ burner-next/wallet/pages/index.js
@@ -1,26 +1,7 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
+import styles from "styles/Home.module.css";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
-
-  const handleMessage = (event) => {
-    if (event && event.data) {
-      const evtIn =
-        typeof event.data === "string" ? JSON.parse(event.data) : event.data;
-      if (evtIn.name) {
-        console.log(`inside: ${evtIn.name}, value: ${evtIn.value}`);
-        const evtOut = { name: "sendOut" };
-        setMessage(evtIn.value);
-      }
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("message", handleMessage);
-  }, []);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -28,7 +9,7 @@ export default function Home() {
         <meta name="description" content="wallet view" />
       </Head>
       <main className={styles.main} style={{ width: "100vw", height: "100vh" }}>
-        {message}
+        Hello
       </main>
     </div>
   );
