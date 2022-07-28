8ba370068fcc6c5c2255c0f100831345f4ddc6aa
0xJilan <0xjilan@protonmail.com>
2022-07-28 09:46:20 +0200

    remove react-query  to handle local storage

--- burner-next/wallet/lib/reply.js
+++
@@ -1,9 +0,0 @@
-const reply = (event) => {
-  if (event) {
-    console.log("from reply");
-
-    console.log(JSON.stringify(event));
-  }
-};
-
-export { reply };
--- burner-next/wallet/pages/index.js
+++ burner-next/wallet/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
-import { reply } from "../lib/reply";
 
 export default function Home() {
   const [message, setMessage] = useState("");
@@ -17,10 +16,6 @@ export default function Home() {
       }
     }
   };
-  const handleClick = () => {
-    console.log("from button");
-    reply({ name: "test" });
-  };
 
   useEffect(() => {
     window.addEventListener("message", handleMessage);
@@ -33,7 +28,6 @@ export default function Home() {
         <meta name="description" content="wallet view" />
       </Head>
       <main className={styles.main} style={{ width: "100vw", height: "100vh" }}>
-        <button onClick={handleClick}>boutton</button>
         {message}
       </main>
     </div>
