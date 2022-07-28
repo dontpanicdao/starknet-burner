817df81af053b4c20b1995234313cad897941ab6
0xJilan <0xjilan@protonmail.com>
2022-07-28 13:12:02 +0200

    hydrate useEffect with logic of localstorage

--- burner-next/wallet/lib/handleLocalStorage.js
+++ burner-next/wallet/lib/handleLocalStorage.js
@@ -2,4 +2,8 @@ const isLocalStorage = (keyName) => {
   return localStorage.getItem(keyName) ? true : false;
 };
 
-export { isLocalStorage };
+const saveInLocalStorage = (keyName, value) => {
+  return localStorage.setItem(keyName, value);
+};
+
+export { isLocalStorage, saveInLocalStorage };
