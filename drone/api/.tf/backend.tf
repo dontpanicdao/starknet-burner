bb059cde0634c8c9885bd5edbe920e329bc282bf
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-07-30 23:03:17 +0200

    remove configuration specific data

--- drone/api/.tf/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    profile        = "resetlogs"
-    bucket         = "bots-registry.blaqkube.io"
-    key            = "state/burner.state"
-    region         = "us-east-1"
-  }
-}
