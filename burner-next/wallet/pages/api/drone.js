4783d15932a49026a25d7a25bb35ea722f942ebe
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-01 15:34:31 +0200

    refacto drone api respons when 204

--- burner-next/wallet/pages/api/drone.js
+++ burner-next/wallet/pages/api/drone.js
@@ -2,7 +2,7 @@ const handler = async (req, res) => {
   const { k } = req.query;
   const resp = await fetch(`${process.env.DRONE_BASE_URL}/${k}`);
   if (resp.status !== 200) {
-    return res.status(204).json("rien");
+    return res.status(204).send("");
   }
   const data = await resp.json();
   res.status(200).json(data);
