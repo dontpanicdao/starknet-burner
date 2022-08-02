07cc3399852b2df03e131d2078ef20828420b532
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-02 15:44:56 +0200

    add form to send starkpill

--- starkpilled/components/Form.js
+++ starkpilled/components/Form.js
@@ -3,11 +3,25 @@ import styles from "../styles/Form.module.css";
 const Form = () => {
   return (
     <form className={styles.form}>
-      <label>
-        Nom :
-        <input type="text" name="name" />
-      </label>
-      <input type="submit" value="Envoyer" />
+      <h2 className={styles.formTitle}>SEND SOME STARKPILLS</h2>
+
+      <input
+        className={styles.input}
+        type="text"
+        name="to"
+        placeholder="Address to transfer"
+      />
+
+      <input
+        className={styles.input}
+        type="number"
+        name="amount"
+        min="1"
+        max="10"
+        placeholder="Amount (1-10)"
+      />
+
+      <input className={styles.send} type="submit" value="Send Pills" />
     </form>
   );
 };
