3fc81d313861998be1cd2784b08b5835950afc8b
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-04 11:35:18 +0200

    add a displaycounter linked to dom

--- burner-next/scratch/index.js
+++ burner-next/scratch/index.js
@@ -2,4 +2,26 @@ const hello = () => {
   return console.log("hello from NPM");
 };
 
-export default hello;
+const setupCounter = (element) => {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(++counter));
+  setCounter(0);
+};
+
+const displayCounter = () => {
+  document.querySelector("#counter").innerHTML = `
+  <div>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;
+
+  setupCounter(document.querySelector("#counter"));
+};
+
+export { hello, displayCounter };
