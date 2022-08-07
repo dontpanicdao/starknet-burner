3252b88f4f44d7d3258d3613277f9659a1603976
Gregory Guillou <10611760+gregoryguillou@users.noreply.github.com>
2022-08-07 11:12:39 +0200

    remove starknet-react and only keep the burner 🪙 (#89)

--- starkpilled/components/Form.js
+++ starkpilled/components/Form.js
@@ -1,44 +1,36 @@
-import { useStarknet, useStarknetInvoke } from "@starknet-react/core";
-import useTokenContract from "../lib/useTokenContract";
-import { toBN } from "starknet/dist/utils/number";
 import styles from "../styles/Form.module.css";
 import { useState } from "react";
 
 const Form = () => {
-  const { account } = useStarknet();
   const [formData, setFormData] = useState({
     address: "",
-    amount: "",
+    amount: 0,
   });
 
-  const handleClick = (type) => {
-    if (type === "cancel") {
-      setFormData({
-        address: "",
-        amount: 0,
-      });
-    }
-    if (type === "faucet") {
-      setFormData({
-        address: account || "Please connect to get Faucet address",
-        amount: 0,
-      });
-    }
+  const cancel = () => {
+    setFormData({
+      address: "",
+      amount: 0,
+    });
   };
 
-  const { contract: transferContract } = useTokenContract();
-  const { invoke: tokenTransferInvoke } = useStarknetInvoke({
-    contract: transferContract,
-    method: "transfer",
-  });
-  const parsedValue = {
-    low: toBN(formData.amount),
-    high: toBN(0),
+  const faucet = () => {
+    setFormData({
+      address: `0x05a87f6bec0b6121e55f291f8e06e6149accd706fb43c725a7f1fd3f3f62aadf`,
+      amount: 1,
+    });
+  };
+
+  const send = ({ args, metadata }) => {
+    if (args.address === "" || args.amount <= 1) {
+      return console.log("invalid address or amount");
+    }
+    console.log(`executing ${metadata?.method}, message: ${metadata?.message}`);
   };
 
   return (
     <form className={styles.form}>
-      <h2 className={styles.formTitle}>SEND SOME STARKPILLS</h2>
+      <h2 className={styles.formTitle}>Send Starkpills</h2>
 
       <input
         className={styles.input}
@@ -61,25 +53,25 @@ const Form = () => {
       />
 
       <div className={styles.buttonsContainer}>
-        <div className={styles.button} onClick={() => handleClick("cancel")}>
+        <div className={styles.button} onClick={cancel}>
           Cancel
         </div>
         <div
           className={styles.button}
           value="Send Pills"
           onClick={() => {
-            tokenTransferInvoke({
-              args: [formData.address, parsedValue],
+            send({
+              args: [formData.address, formData.amount],
               metadata: {
                 method: "Transfer",
-                message: "Transfer Starkpills to another address",
+                message: "Sending Starkpills...",
               },
             });
           }}
         >
           Send
         </div>
-        <div className={styles.button} onClick={() => handleClick("faucet")}>
+        <div className={styles.button} onClick={faucet}>
           Faucet
         </div>
       </div>
--- starkpilled/pages/_app.js
+++ starkpilled/pages/_app.js
@@ -1,16 +1,7 @@
-import {
-  getInstalledInjectedConnectors,
-  StarknetProvider,
-} from "@starknet-react/core";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const connectors = getInstalledInjectedConnectors();
-  return (
-    <StarknetProvider connectors={connectors} autoConnect>
-      <Component {...pageProps} />
-    </StarknetProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
