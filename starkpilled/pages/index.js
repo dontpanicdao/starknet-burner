2eb33a1841b91d2a9f38677719573f71c1d7b548
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-08-07 11:04:20 +0200

    remove references to starknet-react

--- starkpilled/pages/index.js
+++ starkpilled/pages/index.js
@@ -1,37 +1,11 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import styles from "../styles/Home.module.css";
 import Form from "../components/Form";
-import UserBalance from "../components/userBalance";
 import Burner from "../components/Burner";
 import { version } from "../lib/version";
 
 export default function Home() {
-  const Wallet = dynamic(() => import("../components/Wallet"), {
-    ssr: false,
-  });
-
-  const [network, setNetwork] = useState("");
-
-  useEffect(() => {
-    if (window?.starknet) {
-      const timer = setTimeout(() => setNetwork(window.starknet.chainId), 500);
-      return () => clearTimeout(timer);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (window?.starknet) {
-      window.starknet.on("accountsChanged", () => {
-        setNetwork(window?.starknet?.chainId);
-      });
-      window.starknet.on("networkChanged", () => {
-        setNetwork(window?.starknet?.chainId);
-      });
-    }
-  });
-
   return (
     <div className={styles.container}>
       <Head>
@@ -42,16 +16,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Ready to Starkpill?</h1>
-        <p>
-          {network !== "SN_GOERLI" ? "Check current network" : "Goërli Network"}
-        </p>
+        <h1 className={styles.title}>fren, Starkpilled?</h1>
         <Burner />
-        <div />
-        <div className={styles.walletContainer}>
-          <Wallet />
-          <UserBalance />
-        </div>
         <Form />
         <footer className={styles.footer}>version {version} 🥰</footer>
       </main>
