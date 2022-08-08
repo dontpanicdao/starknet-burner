9c79f6526f61f07f611bbe97796d626019b87070
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-08 15:59:44 +0200

    add timer to modal

--- starkpilled/components/Modal.js
+++ starkpilled/components/Modal.js
@@ -1,11 +1,19 @@
 import styles from "../styles/Modal.module.css";
+import { useEffect } from "react";
 
 const Modal = ({ isModal, setIsModal }) => {
   console.log("isModal :", isModal);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsModal(!isModal);
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
   return isModal ? (
     <div className={styles.modal}>
-      Transaction succès !{" "}
+      Transaction success !
       <span className={styles.modalCross} onClick={() => setIsModal(!isModal)}>
         X
       </span>
