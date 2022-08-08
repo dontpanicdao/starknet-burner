1e01a3d5716a6e0ccd17101132dcf1c08771fe05
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-08 16:39:44 +0200

    remove prebuild everywhere 🏗️  (#93)

--- burner/wallet/scripts/prebuild.js
+++ burner/wallet/scripts/prebuild.js
@@ -5,6 +5,6 @@ fs.readFile("package.json", "utf8", (err, data) => {
     console.error(err);
     return;
   }
-  const version = JSON.stringify(data)?.version || "dev";
+  const version = JSON.parse(data)?.version || "dev";
   fs.writeFileSync("lib/version.js", `export const version = "${version}";`);
 });
