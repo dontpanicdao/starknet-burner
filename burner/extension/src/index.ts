0548678cd8d8f6b66a50e0819fdbca31c169a77c
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-18 17:03:44 +0200

    finish to refacto style

--- burner/extension/src/index.ts
+++ burner/extension/src/index.ts
@@ -1,12 +1,31 @@
-import { injectExtension } from "./components/extension";
 import { injectButton } from "./components/button";
 import { injectModal } from "./components/modal";
 import { injectIFrame } from "./components/iframe";
 import { registerWindow, exposeRequest } from "./lib/inpage/window";
+import { containerStyleClicked } from "./lib/ui/styles";
 
 export const keyManager = () => {
   registerWindow();
-  injectExtension();
+  const element = document.querySelector<HTMLDivElement>("#starknetburner");
+  if (!element) {
+    throw new Error("Could not query starknetburner");
+  }
+  element.innerHTML = `
+    <div id="button-burner">connect</div>
+    <div id="modal-wrapper"></div>
+    <iframe id="iframe" allow="clipboard-write"/>
+`;
+  const button = element.querySelector<HTMLButtonElement>("#button-burner");
+  if (!button) {
+    throw new Error("Error on button display");
+  }
+
+  let clicked = false;
+  const handleContainerStyle = () => {
+    element.style.cssText = clicked ? "" : containerStyleClicked;
+    clicked = clicked ? false : true;
+  };
+  button.addEventListener("click", handleContainerStyle);
   injectIFrame();
   injectButton();
   injectModal();
