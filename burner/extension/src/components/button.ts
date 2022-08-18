3d884f23bc68906065262ed4be440efcf123178c
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-18 17:12:20 +0200

    Refacto/UI 🧽 (#103)

--- burner/extension/src/components/button.ts
+++ burner/extension/src/components/button.ts
@@ -1,18 +1,34 @@
-import { walletSVG } from "./svg";
-import { burnerButtonStyle } from "../lib/ui/styles";
+import { walletSVG, closeSVG } from "./svg";
+import { burnerButtonStyle, iFrameButtonStyle } from "../lib/ui/styles";
+import { burnerMessage, messageType, request } from "../lib/inpage/message";
 
-const injectButton = (container: HTMLDivElement): HTMLButtonElement => {
-  const button = container.querySelector<HTMLButtonElement>("#button-burner");
-  if (!button) {
-    throw new Error("undefined Button");
+export const injectButton = (): any => {
+  const button = document.querySelector<HTMLButtonElement>("#button-burner");
+  const iFrame = document.querySelector<HTMLIFrameElement>("#iframe");
+  if (!button || !iFrame) {
+    throw new Error("Error on button display");
   }
   button.style.cssText = burnerButtonStyle;
   button.textContent = "Connect";
   button.innerHTML += walletSVG;
   button.setAttribute("onmouseover", "style.transform='scale(1.05)'");
   button.setAttribute("onmouseout", "style.transform='scale(1)'");
-
-  return button;
+  let clicked = false;
+  const handleButtonClick = () => {
+    const iFramePosition = iFrame.getBoundingClientRect();
+    request({
+      type: messageType.display,
+      data: clicked ? "off" : "on",
+    } as burnerMessage);
+    const { top, left, width, height } = iFramePosition;
+    const iFrameTop = top + height * 0.025;
+    const iFrameLeft = left + width * 0.025;
+    button.style.cssText = clicked
+      ? burnerButtonStyle
+      : iFrameButtonStyle(iFrameTop, iFrameLeft);
+    button.textContent = clicked ? "Connect" : "";
+    button.innerHTML += clicked ? walletSVG : closeSVG;
+    clicked = clicked ? false : true;
+  };
+  button.addEventListener("click", handleButtonClick);
 };
-
-export default injectButton;
--- burner/extension/src/components/iframe.ts
+++ burner/extension/src/components/iframe.ts
@@ -1,16 +1,22 @@
-import { HiddenStyle } from "../lib/ui/styles.js";
+import { isWindowSizeLargerThan } from "../lib/ui/responsive.js";
+import { HiddenStyle, iframeStyle } from "../lib/ui/styles.js";
 
-const injectIFrame = (container: HTMLDivElement): HTMLIFrameElement => {
-  const iFrame = container.querySelector<HTMLIFrameElement>("#iframe");
-  if (!iFrame) {
-    throw new Error("undefined iFrame");
+export const injectIFrame = (): void => {
+  const button = document.querySelector<HTMLButtonElement>("#button-burner");
+  const iFrame = document.querySelector<HTMLIFrameElement>("#iframe");
+
+  if (!button || !iFrame) {
+    throw new Error("Error on iframe display");
   }
   iFrame.style.cssText = HiddenStyle;
   iFrame.src = import.meta.env.DEV
     ? "http://localhost:3000"
     : "https://starknet-burner.vercel.app/";
-
-  return iFrame;
+  let clicked = false;
+  const styleIframe = () => {
+    const size = isWindowSizeLargerThan(768) ? 60 : 90;
+    iFrame.style.cssText = clicked ? HiddenStyle : iframeStyle(size);
+    clicked = clicked ? false : true;
+  };
+  button.addEventListener("click", styleIframe);
 };
-
-export default injectIFrame;
