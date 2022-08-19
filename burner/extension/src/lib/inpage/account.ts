057ec607ebaef49f34f449cc123d229c4cabbb7a
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-08-19 15:59:40 +0200

    remove class

--- burner/extension/src/lib/inpage/account.ts
+++ burner/extension/src/lib/inpage/account.ts
@@ -1,163 +1,171 @@
-import { ZERO } from "starknet/constants";
-import {
-  Abi,
-  Call,
-  InvocationsDetails,
-  Signature,
-  RawCalldata,
-} from "starknet/types/lib";
-import { InvokeFunctionResponse } from "starknet/types/provider";
-import { InvocationsSignerDetails } from "starknet/types/signer";
-import { BigNumberish, toBN, toHex } from "starknet/utils/number";
-import { TypedData } from "starknet/utils/typedData/types";
+import { BigNumberish, toBN } from "starknet/utils/number";
 import { AccountInterface } from "starknet/account/interface";
-import { EstimateFeeDetails, EstimateFee } from "starknet/types/account";
-import { SignerInterface } from "starknet/signer/interface";
-import { Provider } from "./provider";
-import { Signer } from "./signer";
+import {
+  EstimateFeeResponse,
+  GetCodeResponse,
+  InvokeFunctionResponse,
+  GetTransactionResponse,
+  GetBlockResponse,
+  ContractClass,
+  InvokeTransactionReceiptResponse,
+  DeployContractResponse,
+  DeclareContractResponse,
+} from "starknet/types";
 
-type EVENT = [(data: any) => void];
-type EventHandlers = {
-  session_loaded: EVENT;
-};
-export const SESSION_LOADED_EVENT = "SESSION_LOADED_EVENT";
+import { signer } from "./signer";
+import { StarknetChainId } from "starknet/constants";
+import { CallContractResponse } from "starknet";
 
-export class Account extends Provider implements AccountInterface {
-  public signer: SignerInterface;
-
-  constructor(public address: string) {
-    super();
-    this.address = address;
-    this.signer = new Signer();
-  }
-
-  public async getNonce(): Promise<string> {
-    const { result } = await this.callContract({
-      contractAddress: this.address,
-      entrypoint: "get_nonce",
-    });
-    return toHex(toBN(result[0]));
-  }
-
-  public async estimateFee(
-    calls: Call | Call[],
-    { nonce: providedNonce, blockIdentifier }: EstimateFeeDetails = {}
-  ): Promise<EstimateFee> {
-    const transactions = Array.isArray(calls) ? calls : [calls];
-    const nonce = providedNonce ?? (await this.getNonce());
-    const version = toBN("0x0");
-
-    const signerDetails: InvocationsSignerDetails = {
-      walletAddress: this.address,
-      nonce: toBN(nonce),
-      maxFee: ZERO,
-      version,
-      chainId: this.chainId,
+export const account: AccountInterface = {
+  signer,
+  chainId: StarknetChainId.TESTNET,
+  estimateFee: async (calls, EstimateFeeDetails) => {
+    console.log("calls", calls);
+    console.log("EstimateFeeDetails", EstimateFeeDetails);
+    const response: EstimateFeeResponse = {
+      overall_fee: "0",
+      gas_consumed: "0",
+      gas_price: "0",
     };
-
-    const signature = await this.signer.signTransaction(
-      transactions,
-      signerDetails
-    );
-
-    const calldata: RawCalldata = [];
-    const response = await super.getEstimateFee(
-      {
-        contractAddress: this.address,
-        entrypoint: "__execute__",
-        calldata,
-        signature,
-      },
-      blockIdentifier,
-      { version }
-    );
-
-    const suggestedMaxFee = toBN("0x0");
-
-    return {
-      ...response,
-      suggestedMaxFee,
+    return Promise.resolve(response);
+  },
+  address: "0x0",
+  getNonce: async () => {
+    return Promise.resolve("0");
+  },
+  execute: async (call, abis, invocationsDetails) => {
+    console.log("call", call);
+    console.log("abis", abis);
+    console.log("invocationsDetails", invocationsDetails);
+    const response: InvokeFunctionResponse = {
+      transaction_hash: "0x0",
     };
-  }
-
-  /**
-   * Invoke execute function in account contract
-   *
-   * [Reference](https://github.com/starkware-libs/cairo-lang/blob/f464ec4797361b6be8989e36e02ec690e74ef285/src/starkware/starknet/services/api/gateway/gateway_client.py#L13-L17)
-   *
-   * @param calls - one or more calls to be executed
-   * @param abis - one or more abis which can be used to display the calls
-   * @param transactionsDetail - optional transaction details
-   * @returns a confirmation of invoking a function on the starknet contract
-   */
-  public async execute(
-    _: Call | Call[],
-    __: Abi[] | undefined = undefined,
-    ___: InvocationsDetails = {}
-  ): Promise<InvokeFunctionResponse> {
-    return this.invokeFunction(
-      {
-        contractAddress: "0x0",
-        entrypoint: "__execute__",
-        calldata: [],
-        signature: [],
-      },
-      {
-        maxFee: "0x0",
-        version: "0x0",
-      }
-    );
-  }
-
-  public async signMessage(typedData: TypedData): Promise<Signature> {
-    return this.signer.signMessage(typedData, this.address);
-  }
-
-  public async hashMessage(_: TypedData): Promise<string> {
-    return "0x0";
-  }
-
-  /**
-   * Verify a signature of a given hash
-   * @warning This method is not recommended, use verifyMessage instead
-   *
-   * @param hash - JSON object to be verified
-   * @param signature - signature of the JSON object
-   * @returns true if the signature is valid, false otherwise
-   * @throws {Error} if the JSON object is not a valid JSON or the signature is not a valid signature
-   */
-  public async verifyMessageHash(
-    _: BigNumberish,
-    __: Signature
-  ): Promise<boolean> {
-    return true;
-  }
-
-  /**
-   * Verify a signature of a JSON object
-   *
-   * @param hash - hash to be verified
-   * @param signature - signature of the hash
-   * @returns true if the signature is valid, false otherwise
-   * @throws {Error} if the signature is not a valid signature
-   */
-  public async verifyMessage(_: TypedData, __: Signature): Promise<boolean> {
-    return true;
-  }
-
-  public _events: EventHandlers = {
-    session_loaded: [
-      (data: any) => {
-        this.address = data.account;
-      },
-    ],
-  };
-}
-
-export const account = new Account("0x0");
-
-export const addEvent = (type: string, fn: (data: any) => void) => {
-  if (type === SESSION_LOADED_EVENT) {
-    account._events.session_loaded.push(fn);
-  }
+    return Promise.resolve(response);
+  },
+  signMessage: async (typedData) => {
+    console.log("typedData", typedData);
+    return await signer.signMessage(typedData, account.address);
+  },
+  hashMessage: async (message) => {
+    console.log("message", message);
+    return Promise.resolve("0x0");
+  },
+  verifyMessage: async (typedData, signature) => {
+    console.log("typedData", typedData);
+    console.log("signature", signature);
+    return Promise.resolve(true);
+  },
+  verifyMessageHash: async (hash, signature) => {
+    console.log("hash", hash);
+    console.log("signature", signature);
+    return Promise.resolve(true);
+  },
+  callContract: async (call, blockIdentifier) => {
+    console.log("call", call);
+    console.log("blockIdentifier", blockIdentifier);
+    const response: CallContractResponse = {
+      result: ["0x0"],
+    };
+    return Promise.resolve(response);
+  },
+  getCode: async (address, blockIdentifier) => {
+    console.log("address", address);
+    console.log("blockIdentifier", blockIdentifier);
+    const response: GetCodeResponse = {
+      bytecode: ["0x0"],
+    };
+    return Promise.resolve(response);
+  },
+  getBlock: async (blockIdentifier) => {
+    console.log("blockIdentifier", blockIdentifier);
+    const out: GetBlockResponse = {
+      accepted_time: 1599098983,
+      block_hash:
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      block_number: 1,
+      gas_price: "0x1",
+      new_root:
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      parent_hash:
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      sequencer:
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+      status: "REJECTED",
+      transactions: [],
+    };
+    return Promise.resolve(out);
+  },
+  getClassAt: async (contractAddress, blockIdentifier) => {
+    console.log("contractAddress", contractAddress);
+    console.log("blockIdentifier", blockIdentifier);
+    const response: ContractClass = {
+      program: "0x0",
+      entry_points_by_type: {},
+    };
+    return Promise.resolve(response);
+  },
+  getEstimateFee: async (invocation, blockIdentifier, details) => {
+    console.log("invocation", invocation);
+    console.log("blockIdentifier", blockIdentifier);
+    console.log("details", details);
+    const response: EstimateFeeResponse = {
+      overall_fee: toBN(0),
+      gas_consumed: toBN(0),
+      gas_price: toBN(0),
+    };
+    return Promise.resolve(response);
+  },
+  getStorageAt: async (contractAddress, key, blockIdentifier) => {
+    console.log("contractAddress", contractAddress);
+    console.log("key", key);
+    console.log("blockIdentifier", blockIdentifier);
+    const response: BigNumberish = key;
+    return Promise.resolve(response);
+  },
+  getTransaction: async (transactionHash) => {
+    console.log("transactionHash", transactionHash);
+    const response: GetTransactionResponse = {
+      calldata: [],
+    };
+    return Promise.resolve(response);
+  },
+  getTransactionReceipt: async (transactionHash) => {
+    console.log("transactionHash", transactionHash);
+    const response: InvokeTransactionReceiptResponse = {
+      messages_sent: [],
+      transaction_hash: "0x0",
+      events: [],
+      status: "REJECTED",
+    };
+    return Promise.resolve(response);
+  },
+  invokeFunction: async (invocation, details) => {
+    console.log("invocation", invocation);
+    console.log("details", details);
+    const response: InvokeFunctionResponse = {
+      transaction_hash: "0x0",
+    };
+    return Promise.resolve(response);
+  },
+  deployContract: async (payload) => {
+    console.log("payload", payload);
+    const response: DeployContractResponse = {
+      transaction_hash: "0x0",
+      contract_address: "0x0",
+    };
+    return Promise.resolve(response);
+  },
+  declareContract: async (payload) => {
+    console.log("payload", payload);
+    const response: DeclareContractResponse = {
+      transaction_hash: "0x0",
+      class_hash: "0x0",
+    };
+    return Promise.resolve(response);
+  },
+  waitForTransaction: async (txHash, retryInterval) => {
+    console.log("txHash", txHash);
+    console.log("retryInterval", retryInterval);
+    return Promise.resolve();
+  },
 };
