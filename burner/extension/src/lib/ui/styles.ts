2b75fc771ad6d4096d5c07d873a3cb2f736b7865
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-22 10:48:59 +0200

    fix style of button and iframe 🔧 (#124)

--- burner/extension/src/lib/ui/styles.ts
+++ burner/extension/src/lib/ui/styles.ts
@@ -6,5 +6,9 @@ export const modalStyle =
   "display:flex;width:100vw;height:100vh;position:absolute;top:0;left:0;background-color:rgba(0,0,0,0.4);filter:blur(0.6rem);justify-content:center;align-items:center";
 
 export const iframeStyle = (size: number) => {
-  return `display:flex;position:absolute;background-color:white;width:${size}vw;height:${size}vh;z-index:100;border-radius:1rem;border:none;box-shadow:1rem 0.6rem 2rem 0.2rem rgba(0,0,0,0.5);`;
+  return `display:flex;position:absolute;background-color:white;width:${size}vw;height:${size}vh;left:${
+    (100 - size) / 2
+  }%;top:${
+    (100 - size) / 2
+  }%;z-index:100;border-radius:1rem;border:none;box-shadow:1rem 0.6rem 2rem 0.2rem rgba(0,0,0,0.5);`;
 };
